ccb5442d26d14f31eb74b008edde8713d37732e0
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-22 18:42:52 +0400

    -- ConnectionApp.java

--- src/main/java/lesson18/jdbc/ConnectionApp.java
+++ src/main/java/lesson18/jdbc/ConnectionApp.java
@@ -1,9 +1,25 @@
 package lesson18.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ConnectionApp {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws SQLException {
     Connection conn = DbConnection.getConnection();
+
+    final String SQLS = "SELECT * FROM messages";
+    PreparedStatement stmt = conn.prepareStatement(SQLS);
+    ResultSet rset = stmt.executeQuery();
+    // processing data
+    while (rset.next()) {
+      String line = String.format("id: %d, sender:%d, receiver:%d, message:%s",
+          rset.getInt("id"),
+          rset.getInt("user_from"),
+          rset.getInt("user_to"),
+          rset.getString("content"));
+      System.out.println(line);
+    }
   }
 }
