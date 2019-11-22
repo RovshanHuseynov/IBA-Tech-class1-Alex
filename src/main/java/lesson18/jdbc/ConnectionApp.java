71325e53fbf58ae2ee264922dc0be44895bae02b
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-22 19:32:40 +0400

    -- ConnectionApp.java

--- src/main/java/lesson18/jdbc/ConnectionApp.java
+++ src/main/java/lesson18/jdbc/ConnectionApp.java
@@ -9,6 +9,8 @@ public class ConnectionApp {
   public static void main(String[] args) throws SQLException {
     Connection conn = DbConnection.getConnection();
 
+
+
     final String SQLS = "SELECT * FROM messages";
     PreparedStatement stmt = conn.prepareStatement(SQLS);
     ResultSet rset = stmt.executeQuery();
@@ -21,5 +23,12 @@ public class ConnectionApp {
           rset.getString("content"));
       System.out.println(line);
     }
+
+    final String SQLI = "INSERT INTO messages (content, user_from, user_to) values (?, ?, ?)";
+    PreparedStatement stmt_insert = conn.prepareStatement(SQLI);
+    stmt_insert.setString(1, "new message");
+    stmt_insert.setInt(2, 33);
+    stmt_insert.setInt(3, 44);
+    stmt_insert.execute();
   }
 }
