5fa40c73562ad391a064b7d336dee8ce82433902
Alexey <alexey.rykhalskiy@gmail.com>
2019-12-02 17:16:12 +0400

    -- _warmup.txt task

--- src/main/java/lesson22/warmup/ArrayTask.java
+++ src/main/java/lesson22/warmup/ArrayTask.java
@@ -15,15 +15,23 @@ public class ArrayTask {
   public static void main(String[] args) {
     Stream<int[]> step1 = Arrays.stream(data());
     Stream<Integer> step2 = step1
-        .flatMap((int[] ints) -> Arrays.stream(ints).boxed());
+        .flatMap(new Function<int[], Stream<Integer>>() {
+          @Override
+          public Stream<Integer> apply(int[] ints) {
+            IntStream stream = Arrays.stream(ints);
+            Stream<Integer> boxed = stream.boxed();
+            return boxed;
+          }
+        });
     //.sorted();
     IntStream step3 = step2.mapToInt(x -> x);
-
-    IntSummaryStatistics st = step3.summaryStatistics();
-    st.getMax();
-    st.getMin();
-    st.getCount();
-    st.getAverage();
-    st.getSum();
+    step3.sorted().forEach(x -> System.out.print(x + " "));
+//
+//    IntSummaryStatistics st = step3.summaryStatistics();
+//    st.getMax();
+//    st.getMin();
+//    st.getCount();
+//    st.getAverage();
+//    st.getSum();
   }
 }
