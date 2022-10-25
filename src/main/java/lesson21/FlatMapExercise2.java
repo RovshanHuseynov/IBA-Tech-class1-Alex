d0d463b0ed646fe6f66106ea09b4c31927110b0d
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-10-25 20:55:42 +0400

    -- convert nested for loop to flatMap() and map()

--- src/main/java/lesson21/FlatMapExercise2.java
+++ src/main/java/lesson21/FlatMapExercise2.java
@@ -2,10 +2,16 @@ package lesson21;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FlatMapExercise2 {
     public static void main(String[] args) {
         List<Integer> letters = Arrays.asList(1, 2, 3, 4, 5);
-        letters.forEach(i -> letters.forEach(j -> System.out.println(i + " " + j)));
+        letters.forEach(i -> letters.forEach(j -> System.out.println(i + "-" + j)));
+
+        List<String> list = letters.stream()
+                .flatMap(i -> letters.stream().map(j -> i + "-" + j))
+                .collect(Collectors.toList());
+        System.out.println(list);
     }
 }
